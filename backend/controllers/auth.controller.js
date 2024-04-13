6feeef33d65935616d31cda9df8c170e59eb8065
Raihan <raihanromi98@gmail.com>
2024-04-13 16:27:43 +0600

    signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,47 @@
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+
 const signup = async (req, res) => {
   try {
-    const { fullname, username, password, confirmPassword } = req.body;
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Password dont match" });
+    }
+
+    const user = await User.findOne({ username });
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    const salt = bcrypt.salt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    if (newUser) {
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return res.status(400).json({ error: "Invalid user data" });
+    }
   } catch (error) {
-    console.log(error);
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
+
 const connectToMongDB = require("./db/connectToMongoDB");
 const authRoutes = require("./routes/auth.routes");
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("server is ready");
 });
 
+
+app.use(express.json())
 app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
