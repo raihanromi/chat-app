abc8cc6210b4b2618fae4d3043d1a4b79c8b2d4e
Raihan <raihanromi98@gmail.com>
2024-04-13 23:05:49 +0600

    auth feature updated

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcryptjs");
+const generateTokenAndSetCookie = require("../utils/generateToken");
 
 const signup = async (req, res) => {
   try {
@@ -14,7 +15,7 @@ const signup = async (req, res) => {
       return res.status(400).json({ error: "Username already exists" });
     }
 
-    const salt = bcrypt.salt(10);
+    const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
@@ -23,12 +24,14 @@ const signup = async (req, res) => {
     const newUser = new User({
       fullName,
       username,
-      password,
+      password: hashedPassword,
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
 
     if (newUser) {
+      //Generate JWT token
+      generateTokenAndSetCookie(newUser._id, res);
       await newUser.save();
       res.status(201).json({
         _id: newUser._id,
@@ -45,12 +48,42 @@ const signup = async (req, res) => {
   }
 };
 
-const login = (req, res) => {
-  console.log("loginUser");
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ error: "Invalid username" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (isPasswordCorrect) {
+      generateTokenAndSetCookie(user._id, res);
+      res.status(200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        username: username,
+        profilePic: user.profilePic,
+      });
+    } else {
+      return res.status(400).json({ error: "Password is not correct" });
+    }
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
+
 const logout = (req, res) => {
-  console.log("logoutUser");
+  try {
+    res.cookie("jwt","",{maxAge:0})
+    res.status(200).json({message:"Logged out successfully"})
+
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 module.exports = {
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+const generateTokenAndSetCookie = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "strict",
+  });
+};
+
+module.exports= generateTokenAndSetCookie
