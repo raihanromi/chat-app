84467d85f06229329ccbd7cd386abee617140d60
Raihan <raihanromi98@gmail.com>
2024-04-16 03:36:47 +0600

    get messages commit

--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -11,7 +11,7 @@ const conversationSchema = mongoose.Schema(
     messages: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+        ref: "Message",
         default: [],
       },
     ],
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const sendMessage = require("../controllers/message.controller");
+const {sendMessage ,getMessages }= require("../controllers/message.controller");
 const protectRoute = require("../middleware/protectRoute")
 
 router.post("/send/:id", protectRoute,sendMessage);
+
+router.get("/:id", protectRoute,getMessages);
 
 module.exports = router;
